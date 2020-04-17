ea2202e42fa6ccc64f7e824e6e7910b48ef8d390
Marc PEREZ <marc.perez@epitech.eu>
2020-04-17 03:05:10 +0200

    Initial Commit

---
+++ lab6/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  lab6
+//
+//  Created by Marc PEREZ on 17/4/2020.
+//  Copyright © 2020 Marc PEREZ. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ lab6Tests/lab6Tests.swift
@@ -0,0 +1,34 @@
+//
+//  lab6Tests.swift
+//  lab6Tests
+//
+//  Created by Marc PEREZ on 17/4/2020.
+//  Copyright © 2020 Marc PEREZ. All rights reserved.
+//
+
+import XCTest
+@testable import lab6
+
+class lab6Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
