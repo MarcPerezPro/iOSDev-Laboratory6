9864ab4860573a6b252156fe1cd5581cea066b21
Marc PEREZ <marc.perez@epitech.eu>
2020-04-17 03:34:08 +0200

    Adding the image load code

--- lab6/FirstViewController.swift
+++ lab6/FirstViewController.swift
@@ -27,3 +27,19 @@ class FirstViewController: UIViewController {
 
 }
 
+/// Stolen from:
+/// https://www.hackingwithswift.com/example-code/uikit/how-to-load-a-remote-image-url-into-uiimageview
+private extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
+
