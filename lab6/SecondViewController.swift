384324538227c08a0bd70a2c5203f299db9a2b9a
Marc PEREZ <marc.perez@epitech.eu>
2020-04-18 01:51:31 +0200

    Map UI

--- lab6/SecondViewController.swift
+++ lab6/SecondViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import MapKit
 
 class SecondViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var latitudeInput: UITextField!
+    @IBOutlet weak var longitudeInput: UITextField!
+    @IBAction func onGoToTapped(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var destinationLatitudeInput: UITextField!
+    @IBOutlet weak var destinationLongitudeInput: UITextField!
+    @IBAction func onGetRouteTapped(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var transportTypePicker: UIPickerView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//
+//        let geocoder = CLGeocoder()
+//        geocoder.geocodeAddressString(searchBarMap.text!) { (placemarks:[CLPlacemark]?, error:Error?) in
+//            if error == nil {
+//
+//                let placemark = placemarks?.first
+//
+//                let anno = MKPointAnnotation()
+//                anno.coordinate = (placemark?.location?.coordinate)!
+//                anno.title = self.searchBarMap.text!
+//
+//                let span = MKCoordinateSpanMake(0.075, 0.075)
+//                let region = MKCoordinateRegion(center: anno.coordinate, span: span)
+//
+//                self.mapView.setRegion(region, animated: true)
+//                self.mapView.addAnnotation(anno)
+//                self.mapView.selectAnnotation(anno, animated: true)
+//
+//
+//
+//            }else{
+//                print(error?.localizedDescription ?? "error")
+//            }
+//        }
+//    }
+    
 }
 
